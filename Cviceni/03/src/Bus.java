7ec2476e4c9bf9ca833fe05cc8624ed547bda82b
Petr_Holec <petr.holec@tul.cz>
2021-03-18 22:00:08 +0100

    odeslani aplikace bus

--- Cviceni/03/src/Bus.java
+++ Cviceni/03/src/Bus.java
@@ -1,3 +1,78 @@
 public class Bus {
-    
+	//data
+	private int capacity; //vlastnost, nebude se měnit
+	private double speed = 0;
+	private int numberOfStops;
+	private String company;
+	private int numberOfPassangers = 0; //stav, bude se měnit
+	
+	//konstruktor 
+	public Bus(int capacity, int numberOfStops, String company) {
+		this.capacity = capacity;
+		this.numberOfStops = numberOfStops;
+		this.company = company;
+		
+	}
+	
+	//pretizeny konstruktor (overloaded)
+	public Bus() {
+		
+	}
+	
+	//metody
+	//public class interface, public class API
+	//překrytí metody
+	
+	public String toString() {
+		return "company " + company + ", pocet zastavek " + numberOfStops + " ,rychlost " + speed + ", kapacita " + 
+				capacity + ", pocet kastanu " + numberOfPassangers;
+	}
+	
+	public boolean getIn() {
+		if (numberOfPassangers >= capacity){
+			return false;
+		} 
+		
+		numberOfPassangers++;
+		return true;
+	}
+	
+	public void getOf() {
+		if(numberOfPassangers <= 0) {
+			return;
+		}
+		numberOfPassangers--;
+	}
+	
+	public void getOfAll() {
+		//numberOfPassangers = 0;
+		while (numberOfPassangers != 0) {
+			getOf();
+		}
+	}
+	
+	
+	//setter
+	public void setCapacity(int capacity) {
+		if (!(capacity >= numberOfPassangers)) {
+			throw new IllegalArgumentException("kapacita byl měla být větší nebo rovna než počet cestujících");
+		}
+		this.capacity = capacity;
+		
+	}
+	
+	public void setSpeed() {
+		this.speed = speed;
+		
+	}
+	
+	
+	//getter
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
 }
--- Cviceni/03/src/BusApp.java
+++ Cviceni/03/src/BusApp.java
@@ -1,5 +1,53 @@
 public class BusApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Bus universityBus = new Bus();
+		//universityBus.capacity = 42;
+		
+		Bus homeBus = new Bus();
+		Bus universityBus = new Bus(42, 5, "RegioJet");
+		System.out.println(universityBus.toString());
+		System.out.println(homeBus.toString());
+		
+		
+		
+		
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		
+		System.out.println(universityBus.getCapacity());
+		universityBus.setCapacity(100);
+		System.out.println(universityBus.getCapacity());
+		
+		System.out.println("");
+		
+		System.out.println("pokus o nastup");
+		System.out.println(universityBus.getIn());
+		universityBus.getIn();
+		System.out.println(universityBus.toString());
+		
+		System.out.println("");
+		
+		System.out.println("výstup jednoho člověka");
+		universityBus.getOf();
+		System.out.println(universityBus.toString());
+		
+		System.out.println("");
+		
+		System.out.println("výstup všech");
+		universityBus.getOfAll();
+		System.out.println(universityBus.toString());
+		
+		System.out.println("");
+		
+		System.out.println("výstup jednoho");
+		universityBus.getOf();
+		System.out.println(universityBus.toString());
+		
+		
+		
+		
     }
 }
